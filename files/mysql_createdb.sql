c945761cb4b2050dfb44b5823105e6a06150a4e8
Washington Luís Araújo <washinigton.daraujo@sonda.com>
2018-04-06 19:03:16 -0300

    testing | the basis of many cafes

---
+++ files/mysql_createdb.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE roundcubemail /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
+GRANT ALL PRIVILEGES ON roundcubemail.* TO roundcube@localhost INDENTIFIEDY BY '4linux';
+FLUSH PRIVILEGES;
